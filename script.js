b347fbb5a57766a8cf1393bc7ac0a1282cf1b8c0
Percy Kim <106507373+userkim7@users.noreply.github.com>
2023-11-07 22:03:54 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ let isShowing = false;
 document.getElementById("button").addEventListener("click", function () {
     if (isShowing) {
         isShowing = false;
-        popup.style.transition = 'transform 0.3s ease';
+        popup.style.transition = 'transform 1.5s ease';
         popup.style.transform = 'translateY(0px)';
         const image = document.getElementById('arrow');
         image.style.transform = 'rotate(0deg)';
     } else {
         isShowing = true;
-        popup.style.transition = 'transform 0.3s ease';
+        popup.style.transition = 'transform 1.5s ease';
         popup.style.transform = 'translateY(-255px)';
         const image = document.getElementById('arrow');
         image.style.transform = 'rotate(180deg)';
