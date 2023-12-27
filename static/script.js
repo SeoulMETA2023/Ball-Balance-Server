d619be93d0d1af658ca58ed6cc7bcbe7b5867bad
Percy Kim <106507373+userkim7@users.noreply.github.com>
2023-12-27 22:37:20 +0900

    Update script.js

--- static/script.js
+++ static/script.js
@@ -17,3 +17,51 @@ document.getElementById("button").addEventListener("click", function () {
     }
 
 })
+
+function createChart() {
+    var data = {
+        labels: ['', '', '', '', ''],
+        datasets: [
+            {
+                label: 'A',
+                data: [10, 50, 30, 70, 50],
+                borderColor: 'red',
+                fill: false
+            },
+            {
+                label: 'B',
+                data: [10, 90, 5, 40, 60],
+                borderColor: 'blue',
+                fill: false
+            }
+        ]
+    };
+
+    var options = {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+            legend: {
+              display: false,
+            }
+          },
+        scales: {
+            y:{
+                ticks: {
+                    color: '#D9D9D9'
+                }
+            }
+        }
+    };
+
+    var ctx = document.getElementById('myLineChart').getContext('2d');
+    var myLineChart = new Chart(ctx, {
+        type: 'line',
+        data: data,
+        options: options
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    createChart();
+});
